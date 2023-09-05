bd766af27675121cd6697b08aca3a6c13320f518
Matthew <38759997+friendlymatthew@users.noreply.github.com>
2023-09-04 21:34:05 -0400

    New work

--- front/src/pages/chat/index.tsx
+++ front/src/pages/chat/index.tsx
@@ -1,7 +1,17 @@
+import Head from "next/head";
+import Link from "next/link";
+
 export default function ChatPage() {
   return (
-    <div>
-      <div></div>
-    </div>
+    <>
+      <Head>
+        <title>chat.xyz</title>
+        <meta name="description" content="An XYZ service" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="flex flex-col w-full justify-center items-center h-screen">
+
+      </main>
+    </>
   );
 }
--- front/src/pages/chat/states/selectedRoomStateFamily.ts
+++
@@ -1,6 +0,0 @@
-import { atomFamily } from "recoil";
-
-const selectedRoomFamilyState = atomFamily<boolean, string>({
-  key: "selectedRoomStateFamily",
-  default: false,
-});
