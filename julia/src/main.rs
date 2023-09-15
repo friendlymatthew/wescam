6e5be1054385d08b166a82ea78228657bff26412
Matthew <38759997+friendlymatthew@users.noreply.github.com>
2023-09-14 20:17:05 -0400

    docker complete

--- julia/src/main.rs
+++ julia/src/main.rs
@@ -4,7 +4,7 @@ use warp::Filter;
 use scylla::{Session, SessionBuilder};
 use std::error::Error;
 use std::sync::Arc;
-use tracing::info;
+use tracing::{info, warn, error};
 
 #[path = "./db/mod.rs"]
 mod db;
@@ -23,30 +23,52 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let uri = std::env::var("SCYLLA_URI")
         .unwrap_or_else(|_| "127.0.0.1:9042".to_string());
 
-
-    info!("Connecting to {}", uri);
+    info!("Connecting to ScyllaDB at URI: {}", uri);
 
     let session: Session = SessionBuilder::new()
         .known_node(uri)
         .build()
-        .await?;
+        .await
+        .map_err(|e| {
+          error!("Failed to build session: {}", e);
+            e
+        })?;
 
-    db::configs::table_config::create_tables(&session).await?;
+    info!("ScyllaDB session built successfully");
+
+    match db::configs::table_config::create_tables(&session).await {
+        Ok(_) => info!("Successfully created tables"),
+        Err(e) => warn!("Failed to generate julia keyspace and tables: {}", e),
+    }
 
     let session_arc = Arc::new(session);
+    info!("Arc session created.");
 
-    let prepared_entity_queries = db::configs::prepare_entity_query::PreparedEntityQueries::new(session_arc.clone()).await?;
+    let prepared_entity_queries = db::configs::prepare_entity_query::PreparedEntityQueries::new(session_arc.clone()).await
+        .map_err(|e| {
+            error!("Failed to prepare entity queries: {}", e);
+            e
+        })?;
+    info!("Entity queries prepared");
+
     let prepared_entity_queries = Arc::new(prepared_entity_queries);
 
     let entity_route = api::entity_routes::routes(session_arc.clone(), prepared_entity_queries.clone());
+    info!("Entity routes configured");
+
     let bond_route = api::bond_routes::routes(session_arc.clone());
+    info!("Bond routes configured");
 
     let health_route = warp::path!("health")
-        .map(|| format!("Server is healthy"));
+        .map(|| {
+            info!("Health check triggered");
+            format!("Server is healthy")
+        });
 
     let all_routes = health_route.or(entity_route).or(bond_route);
 
 
+    info!("Starting the server at 127.0.0.1:8080");
     warp::serve(all_routes).run(([127, 0, 0, 1], 8080)).await;
 
     Ok(())
