43d52e156c9e3c4995b7a456753257c00403f42c
Matthew <38759997+friendlymatthew@users.noreply.github.com>
2023-09-16 10:40:13 -0400

    current stable build

--- julia/src/db/configs/table_config.rs
+++ julia/src/db/configs/table_config.rs
@@ -1,43 +1,20 @@
 use scylla::Session;
 use std::error::Error;
+use tracing::info;
 
-pub async fn create_tables(session: &Session) -> Result<(), Box<dyn Error>> {
-    session
-        .query(
-            "CREATE KEYSPACE IF NOT EXISTS julia WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 };",
-            (),
-        )
-        .await?;
+pub async fn initialize(session: &Session) -> Result<(), Box<dyn Error>> {
+    let queries = vec![
+        ("generate_keyspace", "CREATE KEYSPACE IF NOT EXISTS julia WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 };"),
+        ("generate_rogues", "CREATE TABLE IF NOT EXISTS julia.rogues (id text PRIMARY KEY, email text);"),
+        ("secondary_rogue_index", "CREATE INDEX IF NOT EXISTS ON julia.rogues (email);"),
+        ("generate_users", "CREATE TABLE IF NOT EXISTS julia.users (id text PRIMARY KEY, email text, name text, pronouns text, class_year text);"),
+        ("generate_bonds", "CREATE TABLE IF NOT EXISTS julia.bonds (id text PRIMARY KEY, creator_id bigint, crush_id bigint, bond_type text, game_status text, created_at timestamp, updated_at timestamp);"),
+    ];
 
-
-    session
-        .query(
-            "CREATE TABLE IF NOT EXISTS julia.rogues (id text PRIMARY KEY, email text);",
-            (),
-        )
-        .await?;
-
-    session
-        .query(
-            "CREATE INDEX IF NOT EXISTS ON julia.rogues (email);",
-            (),
-        )
-        .await?;
-
-
-    session
-        .query(
-            "CREATE TABLE IF NOT EXISTS julia.users (id text PRIMARY KEY, email text, name text, pronouns text, class_year text);",
-            (),
-        )
-        .await?;
-
-    session
-        .query(
-            "CREATE TABLE IF NOT EXISTS julia.bonds (id text PRIMARY KEY, creator_id bigint, crush_id bigint, bond_type text, game_status text, created_at timestamp, updated_at timestamp);",
-            (),
-        )
-        .await?;
+    for &(title, current_query) in &queries {
+        session.query(current_query, ()).await?;
+        info!("{} successful", title)
+    }
 
     Ok(())
 }
--- julia/src/main.rs
+++ julia/src/main.rs
@@ -1,51 +1,46 @@
 extern crate warp;
 
-use warp::Filter;
+use db::configs::{prepare_entity_query, table_config};
 use scylla::{Session, SessionBuilder};
 use std::error::Error;
 use std::sync::Arc;
 use tracing::info;
+use warp::Filter;
 
 #[path = "./db/mod.rs"]
 mod db;
 
-#[path ="./datatype/mod.rs"]
+#[path = "./datatype/mod.rs"]
 mod datatype;
 
-#[path="./api/mod.rs"]
+#[path = "./api/mod.rs"]
 mod api;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-
     tracing_subscriber::fmt::init();
 
-    let uri = std::env::var("SCYLLA_URI")
-        .unwrap_or_else(|_| "127.0.0.1:9042".to_string());
-
+    let uri = std::env::var("SCYLLA_URI").unwrap_or_else(|_| "127.0.0.1:9042".to_string());
 
     info!("Connecting to {}", uri);
 
-    let session: Session = SessionBuilder::new()
-        .known_node(uri)
-        .build()
-        .await?;
+    let session: Session = SessionBuilder::new().known_node(uri).build().await?;
 
-    db::configs::table_config::create_tables(&session).await?;
+    table_config::initialize(&session).await?;
 
     let session_arc = Arc::new(session);
 
-    let prepared_entity_queries = db::configs::prepare_entity_query::PreparedEntityQueries::new(session_arc.clone()).await?;
+    let prepared_entity_queries =
+        prepare_entity_query::PreparedEntityQueries::new(session_arc.clone()).await?;
     let prepared_entity_queries = Arc::new(prepared_entity_queries);
 
-    let entity_route = api::entity_routes::routes(session_arc.clone(), prepared_entity_queries.clone());
+    let entity_route =
+        api::entity_routes::routes(session_arc.clone(), prepared_entity_queries.clone());
     let bond_route = api::bond_routes::routes(session_arc.clone());
 
-    let health_route = warp::path!("health")
-        .map(|| format!("Server is healthy"));
+    let health_route = warp::path!("health").map(|| format!("Server is healthy"));
 
     let all_routes = health_route.or(entity_route).or(bond_route);
-
 
     warp::serve(all_routes).run(([127, 0, 0, 1], 8080)).await;
 
